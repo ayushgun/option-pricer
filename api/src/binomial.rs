010c7da9251b961a1f1e78137a3b086fb6625a53
sriharshak27 <sriharshakocherla27@gmail.com>
2023-01-03 22:22:03 -0500

    🐙 formatting

--- api/src/binomial.rs
+++ api/src/binomial.rs
@@ -1,6 +1,14 @@
 use std::f64;
 
-pub fn call_price(spot: f64, strike: f64, vol: f64, time: f64, rate: f64,  div: f64, steps: i32) -> f64 {
+pub fn call_price(
+    spot: f64,
+    strike: f64,
+    vol: f64,
+    time: f64,
+    rate: f64,
+    div: f64,
+    steps: i32,
+) -> f64 {
     let dt = time / steps as f64;
     let up = (vol * dt.powf(0.5)).exp();
     let p0 = (up * (-div * dt).exp() - (-rate * dt).exp()) / (up.powi(2) - 1.0);
@@ -20,7 +28,15 @@ pub fn call_price(spot: f64, strike: f64, vol: f64, time: f64, rate: f64,  div: 
     price[0]
 }
 
-pub fn put_price(spot: f64, strike: f64, vol: f64, time: f64, rate: f64,  div: f64, steps: i32) -> f64 {
+pub fn put_price(
+    spot: f64,
+    strike: f64,
+    vol: f64,
+    time: f64,
+    rate: f64,
+    div: f64,
+    steps: i32,
+) -> f64 {
     let dt = time / steps as f64;
     let up = (vol * dt.powf(0.5)).exp();
     let p0 = (up * (-div * dt).exp() - (-rate * dt).exp()) / (up.powi(2) - 1.0);
@@ -34,7 +50,7 @@ pub fn put_price(spot: f64, strike: f64, vol: f64, time: f64, rate: f64,  div: f
         for i in 0..=j {
             price[i as usize] = p0 * price[(i + 1) as usize] + p1 * price[i as usize];
             let exercise = strike - spot * up.powi(2 * i - j);
-            price[i as usize] = f64::max(price[i as usize],exercise);
+            price[i as usize] = f64::max(price[i as usize], exercise);
         }
     }
     price[0]
