a719bb18ac8df24b48d9333b519ae977f155580a
Ayush <ayushgundawar@gmail.com>
2023-01-03 22:02:07 -0500

    🧹 Formatted API

--- api/src/black_scholes.rs
+++ api/src/black_scholes.rs
@@ -1,8 +1,8 @@
-use std::f64;
 use statrs::distribution::Normal;
 use statrs::distribution::Univariate;
+use std::f64;
 
-pub fn d1(s: f64, k: f64, v: f64, t: f64, r: f64, q: f64) -> f64 {
+fn d1(s: f64, k: f64, v: f64, t: f64, r: f64, q: f64) -> f64 {
     (f64::ln(s / k) + (r - q + v.powi(2) / 2.0) * t) / (v * t.powf(0.5))
 }
 
@@ -10,12 +10,14 @@ pub fn d2(s: f64, k: f64, v: f64, t: f64, r: f64, q: f64) -> f64 {
     d1(s, k, v, t, r, q) - v * t.powf(0.5)
 }
 
-pub fn black_scholes_call_price(spot: f64, strike: f64, vol: f64, time: f64, ir: f64, q: f64) -> f64 {
+pub fn call_price(spot: f64, strike: f64, vol: f64, time: f64, ir: f64, q: f64) -> f64 {
     let normal = Normal::new(0.0, 1.0).unwrap();
-    spot * normal.cdf(d1(spot, strike, vol, time, ir, q)) * (-q * time).exp() - strike * normal.cdf(d2(spot, strike, vol, time, ir, q)) * (-ir * time).exp()
+    spot * normal.cdf(d1(spot, strike, vol, time, ir, q)) * (-q * time).exp()
+        - strike * normal.cdf(d2(spot, strike, vol, time, ir, q)) * (-ir * time).exp()
 }
 
-pub fn black_scholes_put_price(spot: f64, strike: f64, vol: f64, time: f64, ir: f64, q: f64) -> f64 {
+pub fn put_price(spot: f64, strike: f64, vol: f64, time: f64, ir: f64, q: f64) -> f64 {
     let normal = Normal::new(0.0, 1.0).unwrap();
-    -spot * normal.cdf(-d1(spot, strike, vol, time, ir, q)) * (-q * time).exp() + strike * normal.cdf(-d2(spot, strike, vol, time, ir, q)) * (-ir * time).exp()
+    -spot * normal.cdf(-d1(spot, strike, vol, time, ir, q)) * (-q * time).exp()
+        + strike * normal.cdf(-d2(spot, strike, vol, time, ir, q)) * (-ir * time).exp()
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,12 @@
 mod black_scholes;
 
 fn main() {
-    // test functions
-    println!("{}",black_scholes::black_scholes_call_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02));
-    println!("{}",black_scholes::black_scholes_put_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02));
+    println!(
+        "{}",
+        black_scholes::call_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02)
+    );
+    println!(
+        "{}",
+        black_scholes::put_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02)
+    );
 }
