d4bdda7d2e85ffef950ea93eb09313f7bb1f8b74
Ayush <ayushgundawar@gmail.com>
2023-01-07 12:32:41 -0500

    🧪 Created frontend and HTTP server

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,22 +1,17 @@
-mod binomial;
-mod black_scholes;
+// mod binomial;
+// mod black_scholes;
+use actix_web::{get, App, HttpServer, Responder};
 
-fn main() {
-    // test functions
-    println!(
-        "{}",
-        black_scholes::call_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02)
-    );
-    println!(
-        "{}",
-        black_scholes::put_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02)
-    );
-    println!(
-        "{}",
-        binomial::call_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02, 1000)
-    );
-    println!(
-        "{}",
-        binomial::put_price(500.0, 510.0, 0.2, 1.0, 0.03, 0.02, 1000)
-    );
+#[get("/")]
+async fn greet() -> impl Responder {
+    format!("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting API at http://localhost:8000/");
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("localhost", 8000))?
+        .run()
+        .await
 }
