a8f75ff1ac9317c7b28c7b977e05031eb52181d8
Ayush <ayushgundawar@gmail.com>
2023-01-07 22:27:51 -0500

    🚀 Began API unit tests

---
+++ api/tests/unit_tests.py
@@ -0,0 +1,15 @@
+"""
+A suite of unit tests for the Rust API. Tests all endpoints and
+validates responses and status codes.
+"""
+
+import unittest
+
+
+class TestAPI(unittest.TestCase):
+    def test_black_scholes_call(self):
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main()
