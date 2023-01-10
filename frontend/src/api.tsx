214cb6bcb4acd99acfd42b6f8660710d5914cb9d
Ayush <ayushgundawar@gmail.com>
2023-01-10 17:01:15 -0500

    🚀 Added initial CSS

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,40 +1,44 @@
-import fetch from 'node-fetch';
- 
-interface black_scholes_data {
-   spot: number
-   strike: number
-   vol: number
-   time: number
-   rate: number
-   div: number
+import fetch from "node-fetch";
+
+interface BlackScholes {
+	spot: number;
+	strike: number;
+	vol: number;
+	time: number;
+	rate: number;
+	div: number;
 }
- 
-interface binomial_data {
-   spot: number
-   strike: number
-   vol: number
-   time: number
-   rate: number
-   div: number
-   steps: number
+
+interface Binomial {
+	spot: number;
+	strike: number;
+	vol: number;
+	time: number;
+	rate: number;
+	div: number;
+	steps: number;
 }
- 
-async function getCallBlackScholes(data: black_scholes_data): Promise<any> {
-   const response = await fetch('/call/black_scholes?data=${JSON.stringify(data)}');
-   return await response.json();
+
+async function black_scholes_call(data: BlackScholes): Promise<any> {
+	const response = await fetch(
+		"/call/black_scholes?data=${JSON.stringify(data)}"
+	);
+	return await response.json();
 }
- 
-async function getPutBlackScholes(data: black_scholes_data): Promise<any> {
-   const response = await fetch('/put/black_scholes?data=${JSON.stringify(data)}');
-   return await response.json();
+
+async function black_scholes_put(data: BlackScholes): Promise<any> {
+	const response = await fetch(
+		"/put/black_scholes?data=${JSON.stringify(data)}"
+	);
+	return await response.json();
 }
- 
-async function getCallBinomial(data: binomial_data): Promise<any> {
-   const response = await fetch('/call/binomial?data=${JSON.stringify(data)}');
-   return await response.json();
+
+async function binomial_call(data: Binomial): Promise<any> {
+	const response = await fetch("/call/binomial?data=${JSON.stringify(data)}");
+	return await response.json();
 }
- 
-async function getPutBinomial(data: binomial_data): Promise<any> {
-   const response = await fetch('/put/binomial?data=${JSON.stringify(data)}');
-   return await response.json();
+
+async function binomial_put(data: Binomial): Promise<any> {
+	const response = await fetch("/put/binomial?data=${JSON.stringify(data)}");
+	return await response.json();
 }
