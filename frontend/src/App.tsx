3cee2cb97fb542f350dbe25bf6fa5b837ef27f1f
sriharshak27 <sriharshakocherla27@gmail.com>
2023-01-13 16:11:34 -0500

    🚀 added binomial react component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,7 +20,7 @@ import {
 //   );
 // }
 
-class FlavorForm extends React.Component<
+export class BlackScholesComp extends React.Component<
     {},
     {
         spot: number;
@@ -29,7 +29,8 @@ class FlavorForm extends React.Component<
         time: number;
         rate: number;
         div: number;
-        price: number;
+        callPrice: number;
+        putPrice: number;
     }
 > {
     constructor(props: any) {
@@ -41,7 +42,8 @@ class FlavorForm extends React.Component<
             time: 0,
             rate: 0,
             div: 0,
-            price: 0,
+            callPrice: 0,
+            putPrice: 0,
         };
 
         this.handleSpot = this.handleSpot.bind(this);
@@ -89,6 +91,20 @@ class FlavorForm extends React.Component<
 
         // replace above code with this when it works
         // black_scholes_call({
+        //   spot: this.state.spot,
+        //   strike: this.state.strike,
+        //   vol: this.state.vol,
+        //   time: this.state.time,
+        //   rate: this.state.rate,
+        //   div: this.state.div,
+        // })
+        //   .then((res) => res.json())
+        //   .then((r) => {
+        //     this.setState({
+        //       price: r.price,
+        //     });
+        //   });
+        // black_scholes_put({
         //   spot: this.state.spot,
         //   strike: this.state.strike,
         //   vol: this.state.vol,
@@ -152,20 +168,191 @@ class FlavorForm extends React.Component<
                             value={this.state.div}
                             onChange={this.handleDiv}
                         />
-                        {/* <select value={this.state.value} onChange={this.handleChange}>
-              <option value="grapefruit">Grapefruit</option>
-              <option value="lime">Lime</option>
-              <option value="coconut">Coconut</option>
-              <option value="mango">Mango</option>
-            </select> */}
                     </label>
                     <br />
                     <br />
                     <input type="submit" value="Submit" />
                 </form>
-                <p>Price: {this.state.price}</p>
+                <p>Call Price: {this.state.callPrice}</p>
+                <b />
+                <p>Put Price: {this.state.putPrice}</p>
             </div>
         );
     }
 }
-export default FlavorForm;
+export class BinomialComp extends React.Component<
+    {},
+    {
+        spot: number;
+        strike: number;
+        vol: number;
+        time: number;
+        rate: number;
+        div: number;
+        steps: number;
+        callPrice: number;
+        putPrice: number;
+    }
+> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            spot: 0,
+            strike: 0,
+            vol: 0,
+            time: 0,
+            rate: 0,
+            div: 0,
+            steps: 0,
+            callPrice: 0,
+            putPrice: 0,
+        };
+
+        this.handleSpot = this.handleSpot.bind(this);
+        this.handleStrike = this.handleStrike.bind(this);
+        this.handleVol = this.handleVol.bind(this);
+        this.handleTime = this.handleTime.bind(this);
+        this.handleRate = this.handleRate.bind(this);
+        this.handleDiv = this.handleDiv.bind(this);
+        this.handleSteps = this.handleSteps.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSpot(event: { target: { value: any } }) {
+        this.setState({ spot: event.target.value });
+    }
+    handleStrike(event: { target: { value: any } }) {
+        this.setState({ strike: event.target.value });
+    }
+    handleVol(event: { target: { value: any } }) {
+        this.setState({ vol: event.target.value });
+    }
+    handleTime(event: { target: { value: any } }) {
+        this.setState({ time: event.target.value });
+    }
+    handleRate(event: { target: { value: any } }) {
+        this.setState({ rate: event.target.value });
+    }
+    handleDiv(event: { target: { value: any } }) {
+        this.setState({ div: event.target.value });
+    }
+    handleSteps(event: { target: { value: any } }) {
+        this.setState({ div: event.target.value });
+    }
+
+    handleSubmit(event: { preventDefault: () => void }) {
+        binomial_call({
+            spot: this.state.spot,
+            strike: this.state.strike,
+            vol: this.state.vol,
+            time: this.state.time,
+            rate: this.state.rate,
+            div: this.state.div,
+            steps: this.state.steps,
+        })
+            .then((res) => res.json())
+            .then((r) => {
+                console.log(r.price);
+            });
+
+        // replace above code with this when it works
+        // binomial_put({
+        //   spot: this.state.spot,
+        //   strike: this.state.strike,
+        //   vol: this.state.vol,
+        //   time: this.state.time,
+        //   rate: this.state.rate,
+        //   div: this.state.div,
+        //   steps: this.state.steps,
+        // })
+        //   .then((res) => res.json())
+        //   .then((r) => {
+        //     this.setState({
+        //       price: r.price,
+        //     });
+        //   });
+        // binomial_call({
+        //   spot: this.state.spot,
+        //   strike: this.state.strike,
+        //   vol: this.state.vol,
+        //   time: this.state.time,
+        //   rate: this.state.rate,
+        //   div: this.state.div,
+        //   steps: this.state.steps,
+        // })
+        //   .then((res) => res.json())
+        //   .then((r) => {
+        //     this.setState({
+        //       price: r.price,
+        //     });
+        //   });
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Spot:
+                        <input
+                            type="number"
+                            value={this.state.spot}
+                            onChange={this.handleSpot}
+                        />
+                        <br />
+                        Strike:
+                        <input
+                            type="number"
+                            value={this.state.strike}
+                            onChange={this.handleStrike}
+                        />
+                        <br />
+                        Volatility:
+                        <input
+                            type="number"
+                            value={this.state.vol}
+                            onChange={this.handleVol}
+                        />
+                        <br />
+                        Time:
+                        <input
+                            type="number"
+                            value={this.state.time}
+                            onChange={this.handleTime}
+                        />
+                        <br />
+                        Interest Rate:
+                        <input
+                            type="number"
+                            value={this.state.rate}
+                            onChange={this.handleRate}
+                        />
+                        <br />
+                        Dividend:
+                        <input
+                            type="number"
+                            value={this.state.div}
+                            onChange={this.handleDiv}
+                        />
+                        <br />
+                        Steps:
+                        <input
+                            type="number"
+                            value={this.state.steps}
+                            onChange={this.handleSteps}
+                        />
+                    </label>
+                    <br />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+                <p>Call Price: {this.state.callPrice}</p>
+                <b />
+                <p>Put Price: {this.state.putPrice}</p>
+            </div>
+        );
+    }
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./App";
-import FlavorForm from "./App";
+import { BlackScholesComp, BinomialComp } from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <FlavorForm />
-  </React.StrictMode>
+    <React.StrictMode>
+        <p>Black Scholes Calculator</p>
+        <BlackScholesComp />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>Binomial Calculator</p>
+        <br />
+        <BinomialComp />
+    </React.StrictMode>
 );
